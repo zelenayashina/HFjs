e52a2b12b0fae20c32dd88961a304f8a55a51ee5
zelenayashina <zelenayashina@gmail.com>
2024-03-11 15:40:12 +0300

    test add

---
+++ headFirst/345/bship.js
@@ -0,0 +1,12 @@
+var view = {
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function(location) {
+
+  },
+  displayMiss: function(location) {
+
+  }
+};
---
+++ headFirst/p85/battleship.js
@@ -0,0 +1,32 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while(isSunk == false) {
+  guess = prompt("Ready, aim, fire! (enter a number 0-6): ");
+  if(guess < 0 || guess > 6) {
+    alert("WRONG NUMBER!")
+  } else {
+    guesses = guesses + 1;
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert("HIT!");
+      hits = hits + 1;
+      if (hits == 3) {
+        isSunk = true;
+        alert("you won");
+      }
+    } else {
+      alert("MISS!");
+    }
+  }
+}
+
+var stats = "You took " + guesses + " guesses to sink battleship, " + "which means your shooting accuracy was " + (3/guesses);
+
+alert(stats);
+
