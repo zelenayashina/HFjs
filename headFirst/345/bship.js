6adaa05c31069f02f6c805888e0e0611d2c28a23
zelenayashina <zelenayashina@gmail.com>
2024-03-12 10:54:21 +0300

    test

--- headFirst/345/bship.js
+++ headFirst/345/bship.js
@@ -4,9 +4,54 @@ var view = {
     messageArea.innerHTML = msg;
   },
   displayHit: function(location) {
-
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
   },
   displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
+};
 
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+          { locations: ["24", "34", "44"], hits: ["", "", ""] },
+          { locations: ["10", "11", "12"], hits: ["", "", ""] }
+  ],
+
+  fire: function(guess) {
+
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      locations = ship.locations;
+      var index = locations.indexOf(guess);
+      if (index >=0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view,displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my ship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+      
+    }
+    view.displayMiss(guess);
+    view.displayMessage("MISSED");
+    return false;
+  },
+  isSUnk: function(ship) {
+    for(var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
   }
 };
